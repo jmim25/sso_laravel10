9453b385fa7c9fe131bddcb3f09551860cb36dba
Juan Miguel I. Moreno <juan.miguel.im25@gmail.com>
2023-03-09 10:05:58 -0600

    Initial commit

---
+++ app/Http/Controllers/VisitasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitas;
+use Illuminate\Http\Request;
+
+class VisitasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Visitas $visitas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Visitas $visitas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Visitas $visitas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Visitas $visitas)
+    {
+        //
+    }
+}
